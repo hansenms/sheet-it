4778dbeecdfa4c6ec2464a4c6eed275d1febd0a4
Michael Hansen <mihansen@microsoft.com>
2019-09-14 18:11:11 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var fse = require('fs-extra');
+var path = require('path');
+var https = require('https');
+
+var sheets = JSON.parse(fse.readFileSync('sheets.json'));
+
+function sheetNameToFileName(sheetName)
+{
+    return sheetName.replace('/', '_');
+}
+
+for (var p in sheets)
+{
+    var folder = path.join("out", p);
+    fse.ensureDirSync(folder);
+    sheets[p].forEach(sheetReference => {
+        var file = fse.createWriteStream(path.join(folder, sheetNameToFileName(sheetReference.name) + ".csv"));
+        https.get(sheetReference.url, function(response) {
+            response.pipe(file);
+          });
+    });
+}
+
+/*
+
+var file = fse.createWriteStream(path.join("out", "ADT01.csv"));
+https.get("https://docs.google.com/spreadsheets/d/1kPOewgrlY4Mpi8W3HyLzb5J-tuZnUBAzgsXuhX5lcGo/gviz/tq?tqx=out:csv", function(response) {
+  response.pipe(file);
+});
+*/
